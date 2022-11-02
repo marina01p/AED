3c826bef7021d76c7cbce9c3d24839928511347a
marina01p <56072634+marina01p@users.noreply.github.com>
2022-11-02 17:50:35 +0200

    add task 1

--- Laboratory 2/main.py
+++ Laboratory 2/main.py
@@ -1,8 +1,22 @@
 import pandas as pd
-
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 #1
 dataset = pd.read_csv('Lab_2_data.csv')
 observations = dataset.head(5)
 
-print("\nFirst 20 rows:")
+print("\nFirst 5 rows:")
+print (observations)
+
+# column_names = list(dataset.columns.values)
+# print(column_names)
+
+dataset = dataset.drop(['Unnamed: 0.1', 'Unnamed: 0'], axis=1)
+
+dataset_cols = ['lfp','wc','hc']
+le = LabelEncoder()
+dataset[dataset_cols] = dataset[dataset_cols].apply(le.fit_transform)
+
+print("\nNew dataset | First 5 rows:")
+observations = dataset.head(5)
 print (observations)
