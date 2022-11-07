27c60813cb3f8df71abb028e86195692a38f2e90
marina01p <56072634+marina01p@users.noreply.github.com>
2022-11-08 01:08:30 +0200

    -- add 3

--- Laboratory 2/main.py
+++ Laboratory 2/main.py
@@ -23,6 +23,39 @@ print (observations)
 
 #3
 print("\nSummary statistics:")
-print(dataset.describe())
-print('\nmed')
-print(dataset.median())
+dataset_summary = dataset.agg(
+    {
+        "lfp": ["min", "max", "median", "skew", "mean"],
+        "k5": ["min", "max", "median", "mean", "skew"],
+        "k618": ["min", "max", "median", "mean", "skew"],
+        "age": ["min", "max", "median", "mean", "skew"],
+        "wc": ["min", "max", "median", "mean", "skew"],
+        "hc": ["min", "max", "median", "mean", "skew"],
+        "lwg": ["min", "max", "median", "mean", "skew"],
+        "inc": ["min", "max", "median", "mean", "skew"],
+    }
+)
+
+print(dataset_summary)
+
+dataset.drop(dataset[dataset['lwg'] < 0].index, inplace = True)
+dataset.drop(dataset[dataset['inc'] < 0].index, inplace = True)
+
+print("\nModified table | Summary statistics:")
+new_dataset_summary = dataset.agg(
+    {
+        "lfp": ["min", "max", "median", "skew", "mean"],
+        "k5": ["min", "max", "median", "mean", "skew"],
+        "k618": ["min", "max", "median", "mean", "skew"],
+        "age": ["min", "max", "median", "mean", "skew"],
+        "wc": ["min", "max", "median", "mean", "skew"],
+        "hc": ["min", "max", "median", "mean", "skew"],
+        "lwg": ["min", "max", "median", "mean", "skew"],
+        "inc": ["min", "max", "median", "mean", "skew"],
+    }
+)
+
+print(new_dataset_summary)
+
+#4
+
