7ad5a920c6024937270c1f68e8fba541cf552616
marina01p <56072634+marina01p@users.noreply.github.com>
2022-11-08 02:02:41 +0200

    add - 4, 5, 6, 7

--- Laboratory 2/main.py
+++ Laboratory 2/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
+import statsmodels.api as sm
 from sklearn.preprocessing import LabelEncoder
+from statsmodels.discrete.discrete_model import Probit
 #1
 dataset = pd.read_csv('Lab_2_data.csv')
 observations = dataset.head(5)
@@ -38,11 +42,11 @@ dataset_summary = dataset.agg(
 
 print(dataset_summary)
 
-dataset.drop(dataset[dataset['lwg'] < 0].index, inplace = True)
-dataset.drop(dataset[dataset['inc'] < 0].index, inplace = True)
+dataset.drop(dataset[dataset['lwg'] <= 0].index, inplace = True)
+dataset.drop(dataset[dataset['inc'] <= 0].index, inplace = True)
 
 print("\nModified table | Summary statistics:")
-new_dataset_summary = dataset.agg(
+dataset_summary = dataset.agg(
     {
         "lfp": ["min", "max", "median", "skew", "mean"],
         "k5": ["min", "max", "median", "mean", "skew"],
@@ -55,7 +59,35 @@ new_dataset_summary = dataset.agg(
     }
 )
 
-print(new_dataset_summary)
+print(dataset_summary)
 
 #4
+print("\nNaN values:")
+print(dataset.isnull().sum())
 
+#5
+# for x in dataset.columns:
+#     if (x == 'wc' or x == 'hc'):
+#         continue
+#     plt.hist(dataset[x], color = 'gray', edgecolor = 'black',
+#          bins = int(35000/1000))
+#     plt.axvline(dataset[x].mean(), color='yellow', linestyle='dashed', linewidth=1)
+#     plt.axvline(dataset[x].median(), color='green', linestyle='dashed', linewidth=1)
+#     plt.title("Histogram for " + x + " column")
+#     plt.show()
+
+#6
+# corr = dataset.corr()
+# f, ax = plt.subplots(figsize=(7, 5))
+# cmap = sns.diverging_palette(230, 20, as_cmap=True)
+# sns.heatmap(corr, annot=True, cmap=cmap)
+# plt.show()
+
+#7
+print("\nProbit model:")
+X = dataset[['k5', 'k618', 'age', 'wc', 'hc', 'lwg', 'inc']]
+Y = dataset['lfp']
+X = sm.add_constant(X)
+model = Probit(Y, X.astype(float))
+probit_model = model.fit()
+print(probit_model.summary())
