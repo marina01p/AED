023e1a97ce33be17de4581c9e0add059177f6496
marina01p <56072634+marina01p@users.noreply.github.com>
2022-11-11 13:08:51 +0200

    add whole lab final version

--- Laboratory 2/main.py
+++ Laboratory 2/main.py
@@ -5,6 +5,13 @@ from sklearn import preprocessing
 import statsmodels.api as sm
 from sklearn.preprocessing import LabelEncoder
 from statsmodels.discrete.discrete_model import Probit
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 #1
 dataset = pd.read_csv('Lab_2_data.csv')
 observations = dataset.head(5)
@@ -66,31 +73,45 @@ print("\nNaN values:")
 print(dataset.isnull().sum())
 
 #5
-# for x in dataset.columns:
-#     if (x == 'wc' or x == 'hc'):
-#         continue
-#     plt.hist(dataset[x], color = 'gray', edgecolor = 'black',
-#          bins = int(35000/1000))
-#     plt.axvline(dataset[x].mean(), color='yellow', linestyle='dashed', linewidth=1)
-#     plt.axvline(dataset[x].median(), color='green', linestyle='dashed', linewidth=1)
-#     plt.title("Histogram for " + x + " column")
-#     plt.show()
+for x in dataset.columns:
+    if (x == 'wc' or x == 'hc'):
+        continue
+    plt.hist(dataset[x], color = 'gray', edgecolor = 'black',
+         bins = int(35000/1000))
+    plt.axvline(dataset[x].mean(), color='yellow', linestyle='dashed', linewidth=1)
+    plt.axvline(dataset[x].median(), color='green', linestyle='dashed', linewidth=1)
+    plt.title("Histogram for " + x + " column")
+    plt.show()
 
 #6
-# corr = dataset.corr()
-# f, ax = plt.subplots(figsize=(7, 5))
-# cmap = sns.diverging_palette(230, 20, as_cmap=True)
-# sns.heatmap(corr, annot=True, cmap=cmap)
-# plt.show()
+corr = dataset.corr()
+f, ax = plt.subplots(figsize=(7, 5))
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+sns.heatmap(corr, annot=True, cmap=cmap)
+plt.show()
 
 #7
-print("\nLogistic regression:")
+print("\nLogit regression:")
+x = dataset[['k5', 'k618', 'age', 'wc', 'hc', 'lwg', 'inc']]
+y = dataset[['lfp']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2)
+
+modelLogistic = LogisticRegression()
+modelLogistic.fit(x_train, y_train)
+
+y_pred = modelLogistic.predict(x_test)
+
+x_train = sm.add_constant(x_train)
+logit_model = sm.Logit(y_train, x_train)
+result = logit_model.fit()
+print(result.summary())
 
 #8
 print("\nProbit model:")
-X = dataset[['k5', 'k618', 'age', 'wc', 'hc', 'lwg', 'inc']]
-Y = dataset['lfp']
-X = sm.add_constant(X)
-model = Probit(Y, X.astype(float))
+x = dataset[['k5', 'k618', 'age', 'wc', 'hc', 'lwg', 'inc']]
+y = dataset['lfp']
+x = sm.add_constant(x)
+model = Probit(y, x.astype(float))
 probit_model = model.fit()
 print(probit_model.summary())
